a875a11214a9f84f249ee3cd588ebc2878d43ddc
Jon Adair <jonadair@gmail.com>
2016-04-23 10:17:55 -0400

    Initial Commit

---
+++ lune/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lune
+//
+//  Created by Jon Adair on 4/23/16.
+//  Copyright © 2016 Thinkamingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lune/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lune
+//
+//  Created by Jon Adair on 4/23/16.
+//  Copyright © 2016 Thinkamingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
